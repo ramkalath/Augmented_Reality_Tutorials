2bbc7ee2b14489659a8129e3479ee257dee41a0d
ramkumar <ramkalath@gmail.com>
2018-05-16 14:33:35 +0530

    added comments to the feature_detection_and_matching_OpenCV.py

--- ch3/feature_detection_and_matching_OpenCV.py
+++ ch3/feature_detection_and_matching_OpenCV.py
@@ -13,41 +13,43 @@ import sys
 
 if __name__ == "__main__":
     img_path = sys.argv
-    img1 = cv2.imread(img_path[1])
-    gray_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.imread(img_path[2])
-    gray_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    img1 = cv2.imread(img_path[1])          # read the first image
+    gray_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)      # convert the first image to grey
+    img2 = cv2.imread(img_path[2])          # read the second image
+    gray_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)      # convert the second image to grey
 
-    sift = cv2.SIFT()
-    kp1, des1 = sift.detectAndCompute(gray_img1, None)
-    kp2, des2 = sift.detectAndCompute(gray_img2, None)
+    sift = cv2.SIFT()                       # create a sift object
+    kp1, des1 = sift.detectAndCompute(gray_img1, None)      # compute the keypoints and descriptors for the first image 
+    kp2, des2 = sift.detectAndCompute(gray_img2, None)      # compute the keypoints and descriptors for the second image
 
+    # -------------------------------------------------------------------
+    # feature matching: we will be using the flann based matcher doing a k nearest neighbour match
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
+    search_params = dict(checks = 50) # above three lines sets the parameters for match
 
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des1, des2, k=2)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)  # creating a flann match object
+    matches = flann.knnMatch(des1, des2, k=2)                   # here is where the actual matches happen between the descriptors
 
-    good = []
-    for m, n in matches:
-        if m.distance < 0.7*n.distance:
-            good.append(m)
+    good = []       # collective list of all the good matches
+    for m, n in matches:    
+        if m.distance < 0.7*n.distance:     # setting a treshold for distance between matches
+            good.append(m)                  # if the matches are good the appended to the good list
     
     if len(good) > 10:
-        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2) # retrive the actual source match points
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2) # retriving the destination match points
+    # -------------------------------------------------------------------
 
-    img1_width = int(img1.shape[1])
-    print img1_width
-    appended_image = np.concatenate((img1, img2), axis=1)
+    img1_width = int(img1.shape[1])    # we are going to append the images so width of the first image will be useful when, in the appended image, we draw points
+    appended_image = np.concatenate((img1, img2), axis=1)   # appending images. This wont work for images with discrepant number of rows, I think. But test images are of the same size so i took the easy way out. Hop over to the other code (feature_detection_and_matching.py) for a proper code snippet to append two diffently shaped images.
 
     for i in range(0, len(src_pts)):
-        img1_pos = (int(src_pts[i][0][0]), int(src_pts[i][0][1]))
-        img2_pos = (int(dst_pts[i][0][0])+img1_width, int(dst_pts[i][0][1]))
-        cv2.circle(appended_image, img1_pos, 3, (0, 0, 255), -1)
-        cv2.circle(appended_image, img2_pos, 3, (0, 0, 255), -1)
-        cv2.line(appended_image, img1_pos, img2_pos, (0, 255, 0), 1)
+        img1_pos = (int(src_pts[i][0][0]), int(src_pts[i][0][1]))   # converting src_pts points to int, tuple
+        img2_pos = (int(dst_pts[i][0][0])+img1_width, int(dst_pts[i][0][1]))    # converting dst_pts to int, tuple
+        cv2.circle(appended_image, img1_pos, 3, (0, 0, 255), -1)    # draw a small circle to indicate a red dot
+        cv2.circle(appended_image, img2_pos, 3, (0, 0, 255), -1)    # draw a small circle to indicate a red dot
+        cv2.line(appended_image, img1_pos, img2_pos, (0, 255, 0), 1)    # draw a line between the dots to obtain matches
 
     cv2.imshow("", appended_image)
     cv2.imwrite("appended_matched_image.jpg", appended_image)
