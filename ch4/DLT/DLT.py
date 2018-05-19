b1235c5964aab557e15cf4428a3692f4fc82dd61
ramkumar <ramkalath@gmail.com>
2018-05-19 10:45:16 +0530

    dummy commit

---
+++ ch4/DLT/DLT.py
@@ -0,0 +1,24 @@
+# *****************************************************************************
+# * Filename : DLT.py
+# * Date : 19/May/2018
+# * Author : Ram
+# * Email : ramkalath@gmail.com
+# * Breif Description : Implements Direct Linear Transform.
+# * Detailed Description : Checkout the image file before you go through this code.
+# *****************************************************************************
+
+
+
+# checkout homography_transformation.jpg before you read this code
+
+import numpy as np
+import scipy as sp
+
+
+
+
+if __name__ == "__main__":
+    # initial_points = 
+    # final_points = 
+    m = np.mean()
+    np.std
