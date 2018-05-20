b1562f0e16b486c8a0d1ad10d01e03952f554c68
ramkumar <ramkalath@gmail.com>
2018-05-20 20:35:30 +0530

    chapter 4 code and images

--- ch4/DLT/DLT.py
+++ ch4/DLT/DLT.py
@@ -7,18 +7,24 @@
 # * Detailed Description : Checkout the image file before you go through this code.
 # *****************************************************************************
 
-
-
-# checkout homography_transformation.jpg before you read this code
+# see homography_transformation.jpg before you read this code
 
 import numpy as np
-import scipy as sp
-
-
-
+from sympy import Matrix
 
 if __name__ == "__main__":
-    # initial_points = 
-    # final_points = 
-    m = np.mean()
-    np.std
+    npoints = 4     # since we have only 4 points
+    ip = np.array([[0, 0], [5, 0], [5, 7], [0, 7]])     # initial points before transformation
+    fp = np.array([[1, 1], [5, 2], [5, 7], [1, 8]])     # final points after transformation
+
+    A = []
+    
+    # for each point append two rows of A
+    for pointi in range(0, len(ip)):
+        A.append([ip[pointi][0], ip[pointi][1], 1, 0, 0, 0, -fp[pointi][0]*ip[pointi][0], -fp[pointi][0]*ip[pointi][1], -fp[pointi][0]])
+        A.append([0, 0, 0, ip[pointi][0], ip[pointi][1], 1, -fp[pointi][1]*ip[pointi][0], -fp[pointi][1]*ip[pointi][1], -fp[pointi][1]])
+
+    A = Matrix(A)   # creating a sympy matrix
+    h = np.array(A.nullspace())   # calculating the homography matrix
+    h = h.reshape(3, 3)     # reshaping h to form a 3x3 matrix
+    print h
