0bc306303335c92c479272dee08bd5d707509936
ramkalath <ramkalath@gmail.com>
2018-05-31 12:47:53 +0530

    chessboard code added

---
+++ ch6/chessboard_detection/chessboard_detection.py
@@ -0,0 +1,50 @@
+# *****************************************************************************
+# * Filename : chessboard_detection.py
+# * Date : 31/May/2018
+# * Author : Ram
+# * Email : ramkalath@gmail.com
+# * Breif Description : Implements chessboard detection
+# * Detailed Description : type ./run.sh to run this code
+# *****************************************************************************
+import numpy as np
+import cv2
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# this describes the shape of the chessboard corners we are tracking - outer corners are discarded
+cols = 7
+rows = 6
+
+if __name__ == "__main__":
+    # we need to describe some points that we are going to track
+    # we need something like [[0, 0, 0], [1, 0, 0] . . . .  [cols-1, 0, 0], [0, 1, 0], [1, 1, 0] . . . . [cols-1, rows-1, 0]]
+    # we compile each col separately and stack them together
+    col0 = np.tile(np.arange(0, cols), rows)
+    col1 = np.reshape((np.ones((cols, rows))* np.arange(rows)).T, (1, cols*rows ))[0]
+    col2 = np.zeros(rows*cols)
+    objpts = np.vstack([col0, col1, col2]).T.reshape(rows*cols, 3)
+
+    cap = cv2.VideoCapture(0)
+    count = 0
+    while(count < 1000):  # Here, 10 can be changed to whatever number you like to choose
+        flag, frame = cap.read() # Capture frame-by-frame
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        flag, corners = cv2.findChessboardCorners(gray_frame, (cols, rows), None) # the variable "corners" contains the pixel information of the tracked chessboard corners
+        if flag == True:
+            print count, ") track successful"
+
+        # painting corners on the frame
+        cv2.drawChessboardCorners(frame, (cols,rows), corners, flag)
+        count += 1
+
+        # display
+        cv2.imshow("tracked chessboard", frame)
+        key = cv2.waitKey(10)
+        if(key==27):
+            break
+
+    # release the used resources
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ ch6/chessboard_detection/run.sh
@@ -0,0 +1,1 @@
+python ./chessboard_detection.py
