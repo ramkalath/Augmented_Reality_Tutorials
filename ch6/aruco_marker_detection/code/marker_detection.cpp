2802f1985c90d6f6e828f0cdf6de442fa374a2b9
ramkalath <ramkalath@gmail.com>
2018-06-02 15:51:00 +0530

    dummy commit

--- ch6/aruco_marker_detection/code/marker_detection.cpp
+++ ch6/aruco_marker_detection/code/marker_detection.cpp
@@ -17,24 +17,24 @@
 
 int main(int argc, char **argv)
 {
+	// some variables we will be using later
 	int cam_device_no, size;
-	double ThreshParam1, ThreshParam2;
+	double ThreshParam1 = 10, ThreshParam2 = 10;
 	float probDetect,  markersize;
 
-	cv::Mat frame;
-	aruco::BoardConfiguration boardconfig;
+	
+	cv::Mat frame; // Mat is the data structure that holds an image in opencv-cpp
+	aruco::BoardConfiguration boardconfig; 
 	aruco::CameraParameters camparams;
 	aruco::BoardDetector boarddetect;
 	
-	ThreshParam1 = 10;
-	ThreshParam2 = 10;
-	
 	if(argc < 3)
 	{
-		cerr<<"Not enough arguments.\nUsage is : ./binary videodevice_number BoardConfig.yml CameraConfig.yml [size]"<<endl;
+		std::cerr << "Not enough arguments.\nUsage is : ./binary videodevice_number BoardConfig.yml CameraConfig.yml [size]" << std::endl;
 		return 0;
 	}
 
+	// all the settings that aruco's datastructures/constructors need -----------------------
 	cam_device_no = atoi(argv[1]);
 	cv::VideoCapture cap(cam_device_no);
 	boardconfig.readFromFile(argv[2]);
@@ -44,14 +44,16 @@ int main(int argc, char **argv)
 	markersize = atoi(argv[4]);
 	boarddetect.setParams(boardconfig, camparams, markersize);
 	boarddetect.getMarkerDetector().getThresholdParams(ThreshParam1, ThreshParam2);
+	// --------------------------------------------------------------------------------------
 
 	while(1)
 	{
 		cap >> frame;
 		probDetect = boarddetect.detect(frame); 	// gives the strength of detection
-		size = boarddetect.getDetectedMarkers().size();
+		size = boarddetect.getDetectedMarkers().size(); // amongst the 10 markers on the board, only some might be detected at a particular time
+
 		for (int i=0;i<size;i++)
-			boarddetect.getDetectedMarkers()[i].draw(frame, cv::Scalar(0, 0, 255), 1);
+			boarddetect.getDetectedMarkers()[i].draw(frame, cv::Scalar(0, 0, 255), 1); // For each detection draw the boundaries of the marker and the marker id
 
 		cv::imshow("", frame);
 		int key = cv::waitKey(10);
