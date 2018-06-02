643c12bdafde0e4fe5aa28eb452ae4aa226ae3f0
ramkalath <ramkalath@gmail.com>
2018-06-02 14:47:16 +0530

    changed some filenames

--- ch6/aruco_marker_detection/code/run.sh
+++ ch6/aruco_marker_detection/code/run.sh
@@ -1,2 +1,2 @@
 # <binary> <cam_device_no> <board_configuration.yml> <camera_parameters.yml> <size_of_square>
-./main 0 board_config.yml camera_parameters.yml 0.025
+./marker_detection 0 board_config.yml camera_parameters.yml 0.025
