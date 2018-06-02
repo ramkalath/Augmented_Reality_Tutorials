eb27fba58ca575a58123e8e2635f42aa2f6902a0
ramkalath <ramkalath@gmail.com>
2018-06-02 10:53:24 +0530

    marker detection with aruco

--- ch6/aruco_marker_detection/code/main.cpp
+++ ch6/aruco_marker_detection/code/main.cpp
@@ -1,10 +1,11 @@
 /*****************************************************************************
- * Author : Ram
- * Date : <date>
+ * Author : Ramkumar
+ * Date : 02/June/2018
  * Email : ramkalath@gmail.com
- * Breif Description :
- * Detailed Description :
+ * Breif Description : demo of marker detection with aruco
+ * Detailed Description : To compile type 'make' and to run type 'run.sh'
  *****************************************************************************/
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -13,32 +14,26 @@
 #include <strings.h>
 #include <vector>
 
-
 int main(int argc, char **argv)
 {
 	int cam_device_no, key, size, row, column;
 	double ThreshParam1, ThreshParam2;
 	float probDetect,  markersize;
+
 	cv::Mat frame;
 	aruco::BoardConfiguration boardconfig;
 	aruco::CameraParameters camparams;
 	aruco::BoardDetector boarddetect;
-	std::vector<cv::Point3f> object_points;
-	std::vector<cv::Point2f> image_points;
-	std::vector<cv::Point3f> cube_obj_pts;
-	std::vector<cv::Point2f> cube_img_pts;
 	
-	cv::Mat rvecs(3, 1, cv::DataType<double>::type);
-	cv::Mat tvecs(3, 1, cv::DataType<double>::type);
-
 	ThreshParam1 = 10;
 	ThreshParam2 = 10;
 	
 	if(argc < 3)
 	{
-		cerr<<"Not enough arguments.\nUsage is : ./binary videodevice_number          boardConfig.yml intrinsics.yml [size] [out]"<<endl;
+		cerr<<"Not enough arguments.\nUsage is : ./binary videodevice_number BoardConfig.yml CameraConfig.yml [size]"<<endl;
 		return 0;
 	}
+
 	cam_device_no = atoi(argv[1]);
 	cv::VideoCapture cap(cam_device_no);
 	boardconfig.readFromFile(argv[2]);
@@ -48,40 +43,14 @@ int main(int argc, char **argv)
 	markersize = atoi(argv[4]);
 	boarddetect.setParams(boardconfig, camparams, markersize);
 	boarddetect.getMarkerDetector().getThresholdParams(ThreshParam1, ThreshParam2);
-	//***Define cube object points *************************************************
-	cv::Point3f c1(0, 0, 0),
-			c2(11, 0, 0),
-			c3(11, 11, 0),
-			c4(0, 11, 0),
-			c5(0, 0, -11),
-			c6(11, 0, -11),
-			c7(11, 11, -11),
-			c8(0, 11, -11);
-	cube_obj_pts.push_back(c1);
-	cube_obj_pts.push_back(c2);
-	cube_obj_pts.push_back(c3);
-	cube_obj_pts.push_back(c4);
-	cube_obj_pts.push_back(c5);
-	cube_obj_pts.push_back(c6);
-	cube_obj_pts.push_back(c7);
-	cube_obj_pts.push_back(c8);
-
-	std::cout << boardconfig.getIdList();
 
 	while(1)
 	{
-		//cap >> frame;
-		frame = cv::imread("webcam_snapshot.jpg");
-		probDetect = boarddetect.detect(frame);
+		cap >> frame;
+		probDetect = boarddetect.detect(frame); 	// gives the strength of detection
 		size = boarddetect.getDetectedMarkers().size();
-		object_points.clear();
-		image_points.clear();
-		std::cout << boarddetect.getDetectedMarkers()[0] << std::endl;
-		//for (int i=0;i<size;i++)
-		//{
-			//boarddetect.getDetectedMarkers()[i].draw(frame, cv::Scalar(0,0,255),1);
-			//std::cout << boarddetect.getDetectedMarkers()[i].x
-		//}
+		for (int i=0;i<size;i++)
+			boarddetect.getDetectedMarkers()[i].draw(frame, cv::Scalar(0, 0, 255), 1);
 
 		cv::imshow("", frame);
 		key = cv::waitKey(10);
--- ch6/aruco_marker_detection/code/run.sh
+++ ch6/aruco_marker_detection/code/run.sh
@@ -1,1 +1,2 @@
+# <binary> <cam_device_no> <board_configuration.yml> <camera_parameters.yml> <size_of_square>
 ./main 0 board_config.yml camera_parameters.yml 0.025
