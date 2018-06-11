fbb3416f825eb2edc801f1c6017a9a2e81608c2e
ramkalath <ramkalath@gmail.com>
2018-06-11 17:05:21 +0530

    variable names change in ch7/extrinsics/extrinsics.py

--- ch7/extrinsics/extrinsics.py
+++ ch7/extrinsics/extrinsics.py
@@ -21,21 +21,18 @@ if __name__ == "__main__":
     col0 = np.tile(np.arange(0, pattern_size[0]), pattern_size[1])
     col1 = np.reshape((np.ones((pattern_size[0], pattern_size[1]))* np.arange(pattern_size[1])).T, (1, pattern_size[0]*pattern_size[1] ))[0]
     col2 = np.zeros(pattern_size[1]*pattern_size[0])
-    pattern_points = np.float32(np.vstack([col0, col1, col2]).T.reshape(pattern_size[1]*pattern_size[0], 3)) # a small addendum, converted float64 into float32
+    object_points = np.float32(np.vstack([col0, col1, col2]).T.reshape(pattern_size[1]*pattern_size[0], 3)) # a small addendum, converted float64 into float32
 
-    stream = open("./camera_parameters.yml", "r") # read the intrinsic matrix from the camera_parameters.yml file
+    stream = open("./cameraConfig.yml", "r") # read the intrinsic matrix from the camera_parameters.yml file
     stream_info = yaml.load(stream)
     intrinsic_matrix = np.array(stream_info["camera_matrix"]["data"]).reshape(3, 3) # we obtain the camera matrix 
     dist_coefs = np.array(stream_info["distortion_coefficients"]["data"]) # we also obtain the distortion coefficients
-
-    axis = np.float32([[0,0,0], [3,0,0], [0,3,0], [0,0,-3]]) # the points to be drawn are define here as axis points
-    axis1 = np.concatenate((axis, np.array([[1],[1],[1],[1]])), axis=1) # they are then converted into homogeneous coordinates
 
     while True:
         flag, frame = cap.read()
         found, corners = cv2.findChessboardCorners(frame, pattern_size) # corners of the chessboard are then detected. We dont find cornerSubPix() because its an expensive operation and number of positive hits are much less compared to the gross findChessboardCorners() method. Also its an overkill
         if(found):
-            rvecs, tvecs, inliers = cv2.solvePnPRansac(pattern_points, corners, intrinsic_matrix, dist_coefs) # we use solvePnP() method to calculate the extrinsics -> rvecs and tvecs
+            rvecs, tvecs, inliers = cv2.solvePnPRansac(object_points, corners, intrinsic_matrix, dist_coefs) # we use solvePnP() method to calculate the extrinsics -> rvecs and tvecs
             rotation_matrix = cv2.Rodrigues(rvecs)[0] # then we convert the quaternions rvecs into the rotation matrix
             extrinsic_matrix =  np.concatenate((rotation_matrix, tvecs), axis = 1) # tvecs can be appended onto the rotation matrix
 
