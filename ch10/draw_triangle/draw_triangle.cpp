e17756d0282aed00189733cf2f84fa252c1b1fd6
ramkalath <ramkalath@gmail.com>
2018-07-05 19:41:44 +0530

    dummy commit

---
+++ ch10/draw_triangle/draw_triangle.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+const GLchar* vertexShaderSource =  "#version 330 core\n"
+                                    "layout (location = 0) in vec3 position;\n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+                                    "}\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+                                     "out vec4 color;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                     "}\n\0";
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+    // When a user presses the escape key, we set the WindowShouldClose property to true, thus closing the application
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        cout << "Window is closing because u pressed the ESC key" << endl;
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+int main()
+{
+    // initializing glfw*****************************************************************
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if(window == nullptr)
+    {
+        cout << "Failed to create a GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // initializing glew*******************************************************************
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK)
+    {
+        cout << "Failed to initialize GLEW" << endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+    glfwSetKeyCallback(window, key_callback);
+
+    // build a shader program***************************************************************
+    // Vertex Shader
+            GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vs, 1, &vertexShaderSource, NULL);
+            glCompileShader(vs);
+
+            // check for compile time errors
+            GLint success;
+            GLchar infoLog[512];
+            glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+            if(!success)
+            {
+                glGetShaderInfoLog(vs, 512, NULL, infoLog);
+                cout << "ERROR: Vertex shader compilation failed." << infoLog << endl;
+            }
+
+    // Fragment Shader
+            GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fs, 1, &fragmentShaderSource, NULL);
+            glCompileShader(fs);
+
+            // check for compile time errors
+            glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
+            if(!success)
+            {
+                glGetShaderInfoLog(fs, 512, NULL, infoLog);
+                cout << "ERROR: Fragment shader compilation failed." << infoLog << endl;
+            }
+
+    // Linking the Shaders
+            GLuint program = glCreateProgram();
+            glAttachShader(program, vs);
+            glAttachShader(program, fs);
+            glLinkProgram(program);
+
+            // check for linking errors
+            glGetProgramiv(program, GL_LINK_STATUS, &success);
+            if(!success)
+            {
+                glGetProgramInfoLog(program, 512, NULL, infoLog);
+                cout << "ERROR: Shader program failed to link" << infoLog << endl;
+            }
+
+    // Delete shaders after compilation
+            glDeleteShader(vs);
+            glDeleteShader(fs);
+    //****************************************************************************************
+    // let us now write code for the actual data points for the triangle and add bind it with a VBO. VAO is then used to encapsulate VBO
+    GLfloat vertices[] = { -0.5f, -0.5f, 0.0f,   /* left */
+                            0.5f, -0.5f, 0.0f,   /* right */
+                            0.0f,  0.5f, 0.0f    /* Top */      };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO); // Bind vertex array objects first before VBOs
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GL_FLOAT), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);               // unbinding VAO
+
+    //**********************************************************************************************
+    // Game Loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // glfw main loop ************************************************
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw our first triangle
+        glUseProgram(program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+     }
+    // Deleting the used resources*****************************************
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    //****************************************************************************
+    glfwTerminate();
+    return 0;
+}
